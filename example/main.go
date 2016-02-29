6a76759a5464f7f3a94e0d2824dd80caf3071d9b
saturn4er <saturn4er@gmail.com>
2016-02-29 02:32:03 +0200

    Fix example after renaming

--- example/main.go
+++ example/main.go
@@ -5,7 +5,7 @@ import (
 	_ "github.com/saturn4er/beego-assets/less"
 	_ "github.com/saturn4er/beego-assets/sass"
 	_ "github.com/saturn4er/beego-assets/scss"
-	_ "github.com/saturn4er/beego-assets/coffeescript"
+	_ "github.com/saturn4er/beego-assets/coffee"
 	_ "github.com/saturn4er/beego-assets/example/routers"
 	"github.com/astaxie/beego"
 )
